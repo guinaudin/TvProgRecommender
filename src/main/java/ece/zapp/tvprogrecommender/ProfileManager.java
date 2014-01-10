460fddfdd40a6d64ab8441f572d6a2bd6815f4dd
Guillaume Naudin <guillaume.naudin78@gmail.com>
2014-01-10 15:56:46 +0100

    database connection + create actorPreferences.csv

--- src/main/java/ece/zapp/tvprogrecommender/ProfileManager.java
+++ src/main/java/ece/zapp/tvprogrecommender/ProfileManager.java
@@ -1,12 +1,41 @@
 package ece.zapp.tvprogrecommender;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 public class ProfileManager {
     //Déclaration des variables
     private Connection myCon;
+    
+    //Statement
+    private Statement stateListArtistPreferences;
+    //Prepare statement
+    //private PreparedStatement pStateListSeats;
+    //Resultat
+    private ResultSet result;
+    //List
+    //private List<Journey> journeyList;
     
     public ProfileManager(String url, String user, String password) throws SQLException {
         //Etablissement de la connection à la BDD
@@ -14,5 +43,46 @@ public class ProfileManager {
         myCon = DriverManager.getConnection(url, user, password);
         //Pas d'auto commit
         myCon.setAutoCommit(false);
+    }
+    
+    public List<RecommendedItem> findActorPreferences() throws SQLException {
+        List<RecommendedItem> recommendations = null;
+        String query = "SELECT userId, artistId, artistWeight FROM ArtistPreferences";
+
+        //On créé la statement
+        stateListArtistPreferences = myCon.createStatement();
+        //On execute la quiery
+        result = stateListArtistPreferences.executeQuery(query);
+        //On commit pour mettre à jour la BDD
+        myCon.commit();
+        try {
+            String filePath = System.getProperty("user.dir") + "/actorPreferences.csv";
+            System.out.println(filePath);
+            FileWriter fileWriter = new FileWriter(filePath, false);
+            BufferedWriter output = new BufferedWriter(fileWriter);
+
+            while(result.next()) {
+                output.write(result.getInt(1) + "," + result.getInt(2) + "," + result.getFloat(3));
+                output.flush();
+            }
+            
+            output.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ProfileManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+            DataModel model = new FileDataModel(new File("actorPreferences.csv"));
+            UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+            UserNeighborhood neighborhood = new NearestNUserNeighborhood(2, similarity, model);
+            Recommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+            recommendations = recommender.recommend(1, 1);
+        } catch (IOException ex) {
+            Logger.getLogger(ProfileManager.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (TasteException ex) {
+            Logger.getLogger(ProfileManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return recommendations;
     }
 }
--- src/main/java/ece/zapp/tvprogrecommender/TvProgRecommender.java
+++ src/main/java/ece/zapp/tvprogrecommender/TvProgRecommender.java
@@ -1,25 +1,23 @@
 package ece.zapp.tvprogrecommender;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 
 public class TvProgRecommender {
 
     public static void main(String[] args) {
         try {
-            ProfileManager profileManager = new ProfileManager("jdbc:mysql://localhost:3306/profile", "root", "");
+            ProfileManager profileManager = new ProfileManager("jdbc:mysql://localhost:3306/Profile", "root", "");
+            List<RecommendedItem> recommendations = profileManager.findActorPreferences();
+
+            for (RecommendedItem recommendation : recommendations) {
+                System.out.println(recommendation);
+            }
         } catch (SQLException ex) {
             Logger.getLogger(TvProgRecommender.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        /*DataModel model = new FileDataModel(new File("intro.csv"));
-        UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
-        UserNeighborhood neighborhood = new NearestNUserNeighborhood(2, similarity, model);
-        Recommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
-        List<RecommendedItem> recommendations = recommender.recommend(1, 1);
-        for (RecommendedItem recommendation : recommendations) {
-            System.out.println(recommendation);*/
     }
 }
