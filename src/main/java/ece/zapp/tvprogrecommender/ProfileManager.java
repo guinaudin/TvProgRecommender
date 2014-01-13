d9b9d79b5eedbbbd18dbb4677cb54827bd6790b5
guinaudin <guillaume.naudin78@gmail.com>
2014-01-13 17:48:27 +0100

    Handle multiple users' recommendations

--- src/main/java/ece/zapp/tvprogrecommender/ProfileManager.java
+++ src/main/java/ece/zapp/tvprogrecommender/ProfileManager.java
@@ -1,26 +1,23 @@
 package ece.zapp.tvprogrecommender;
 
-import java.sql.*;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
-import org.apache.mahout.cf.taste.impl.recommender.knn.ConjugateGradientOptimizer;
-import org.apache.mahout.cf.taste.impl.recommender.knn.Optimizer;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
@@ -29,48 +26,57 @@ import org.apache.mahout.cf.taste.recommender.Recommender;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 public class ProfileManager {
-    //Déclaration des variables
     private Connection myCon;
-    
-    //Statement
     private Statement stateListArtistPreferences;
-    //Prepare statement
-    //private PreparedStatement pStateListSeats;
-    //Resultat
+    private Statement stateListNumberUsers;
     private ResultSet result;
-    //List
-    //private List<Journey> journeyList;
-    
+    private int numberUsers;
+
     public ProfileManager(String url, String user, String password) throws SQLException {
         //Etablissement de la connection à la BDD
         System.setProperty("jdbc.drivers", "com.mysql.jdbc.Driver");
         myCon = DriverManager.getConnection(url, user, password);
         //Pas d'auto commit
         myCon.setAutoCommit(false);
+        
+        numberUsers = this.getNumberUsers();
     }
-    
-    public void weightCalculation () throws SQLException{
-        
+
+    public void weightCalculation() throws SQLException {
+
         Statement stmt = myCon.createStatement();
-        
+
         //récupère tous les id utilisateurs
         ResultSet rs = stmt.executeQuery(
-        "SELECT USERID FROM USERS"); 
-        
-        while(rs.next()){
-           //récupère tous les id des programmes vu pour un utilisateur
-           ResultSet rs2 = stmt.executeQuery(
-           "SELECT PROGID"
-           + "WHERE USERID LIKE 'rs.getLong(0)' FROM USERSHISTORIC");
+                "SELECT USERID FROM USERS");
+
+        while (rs.next()) {
+            //récupère tous les id des programmes vu pour un utilisateur
+            ResultSet rs2 = stmt.executeQuery(
+                    "SELECT PROGID"
+                    + "WHERE USERID LIKE 'rs.getLong(0)' FROM USERSHISTORIC");
         }
-        
+
         //ex de data à rentrer après calcul
-        stmt.executeUpdate("INSERT INTO ARTISTPREFERENCES VALUES (1, 1, 0.99)"); 
-        stmt.executeUpdate("INSERT INTO ARTISTPREFERENCES VALUES (2, 1, 0.19)"); 
+        stmt.executeUpdate("INSERT INTO ARTISTPREFERENCES VALUES (1, 1, 0.99)");
+        stmt.executeUpdate("INSERT INTO ARTISTPREFERENCES VALUES (2, 1, 0.19)");
     }
     
-    public List<RecommendedItem> findActorPreferences() throws SQLException {
-        List<RecommendedItem> recommendations = null;
+    private int getNumberUsers() throws SQLException {
+        String query = "SELECT userId FROM Users";
+
+        stateListNumberUsers = myCon.createStatement();
+        result = stateListNumberUsers.executeQuery(query);
+        myCon.commit();
+        result.last();
+
+        return result.getRow();
+    }
+
+    public Map<Integer, List<RecommendedItem>> findArtistPreferences(int recommendationNumber) throws SQLException {
+        Map<Integer, List<RecommendedItem>> usersArtistRecommendations;
+        usersArtistRecommendations = new HashMap<Integer, List<RecommendedItem>>();
+        List<RecommendedItem> artistRecommendations = null;
         boolean nextResult = false;
         String query = "SELECT userId, artistId, artistWeight FROM ArtistPreferences";
 
@@ -80,16 +86,16 @@ public class ProfileManager {
         result = stateListArtistPreferences.executeQuery(query);
         //On commit pour mettre à jour la BDD
         myCon.commit();
-        
+
         nextResult = result.next();
-        if(nextResult) {
+        if (nextResult) {
             try {
                 String filePath = System.getProperty("user.dir") + "/ArtistPreferences.csv";
                 FileWriter fileWriter = new FileWriter(filePath, false);
                 BufferedWriter output = new BufferedWriter(fileWriter);
 
-                while(nextResult) {
-                    output.write(result.getInt(1) + "," + result.getInt(2) + "," + result.getFloat(3)+"\n");
+                while (nextResult) {
+                    output.write(result.getInt(1) + "," + result.getInt(2) + "," + result.getFloat(3) + "\n");
                     output.flush();
                     nextResult = result.next();
                 }
@@ -98,21 +104,25 @@ public class ProfileManager {
             } catch (IOException ex) {
                 Logger.getLogger(ProfileManager.class.getName()).log(Level.SEVERE, null, ex);
             }
+            
+            try {
+                //faire test si fichier vide
+                DataModel model = new FileDataModel(new File("ArtistPreferences.csv"));
+                UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+                //how many neighbours ??????? (2)
+                UserNeighborhood neighborhood = new NearestNUserNeighborhood(2, similarity, model);
+                Recommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+                for(int i = 1; i <= numberUsers; i++) {
+                    artistRecommendations = recommender.recommend(i, recommendationNumber);
+                    usersArtistRecommendations.put(i, artistRecommendations);
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(ProfileManager.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (TasteException ex) {
+                Logger.getLogger(ProfileManager.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
 
-        try {
-            //faire test si fichier vide
-            DataModel model = new FileDataModel(new File("ArtistPreferences.csv"));
-            UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
-            UserNeighborhood neighborhood = new NearestNUserNeighborhood(2, similarity, model);
-            Recommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
-            recommendations = recommender.recommend(2, 1);
-        } catch (IOException ex) {
-            Logger.getLogger(ProfileManager.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (TasteException ex) {
-            Logger.getLogger(ProfileManager.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        return recommendations;
+        return usersArtistRecommendations;
     }
 }
--- src/main/java/ece/zapp/tvprogrecommender/TvProgRecommender.java
+++ src/main/java/ece/zapp/tvprogrecommender/TvProgRecommender.java
@@ -2,6 +2,7 @@ package ece.zapp.tvprogrecommender;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
@@ -11,11 +12,15 @@ public class TvProgRecommender {
     public static void main(String[] args) {
         try {
             ProfileManager profileManager = new ProfileManager("jdbc:mysql://localhost:3306/Profile", "root", "");
-            List<RecommendedItem> recommendations = profileManager.findActorPreferences();
-            for(RecommendedItem recommendation : recommendations) {
-                System.out.println("recommendation =" + recommendation);
+            Map<Integer, List<RecommendedItem>> usersArtistRecommendations = profileManager.findArtistPreferences(2);
+            for(Map.Entry<Integer, List<RecommendedItem>> entry : usersArtistRecommendations.entrySet()) {
+                for(RecommendedItem recommendation : entry.getValue()) {
+                    System.out.println("recommendation " + entry.getKey() + " = " + recommendation);
+                }
+                System.out.println("");
             }
-        } catch (SQLException ex) {
+        }
+        catch (SQLException ex) {
             Logger.getLogger(TvProgRecommender.class.getName()).log(Level.SEVERE, null, ex);
         }
 
